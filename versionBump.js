7f1a0de9bbcbc94c1cc1c2b9cc5b423e87d87d7b
Blake Byrnes <blakebyrnes@gmail.com>
2022-10-03 13:17:08 -0400

    v1.0.20

--- versionBump.js
+++ versionBump.js
@@ -1,51 +1,51 @@
 const { writeFileSync, readFileSync } = require('fs');
-const { spawn } = require('child_process');
+const { spawnSync } = require('child_process');
 
 const baseDir = process.cwd();
 const pkgJson = readFileSync(`${baseDir}/package.json`, 'utf-8');
 const modified = JSON.parse(pkgJson);
-modified.workspaces.packages = modified.workspaces?.packages?.filter(
-  x => !x.startsWith('..') && !x.includes('/build'),
-);
-(async () => {
-  try {
-    console.log('Removing external workspaces from package.json at: ', baseDir); // eslint-disable-line no-console
-    writeFileSync(`${baseDir}/package.json`, JSON.stringify(modified, null, 2));
-    process.once('beforeExit', () => writeFileSync(`${baseDir}/package.json`, pkgJson));
-    let child;
-    if (!modified.workspaces?.packages?.length) {
-      let version = process.argv[2] ?? '--patch';
-      if (version.match(/^\d/)) {
-        version = '--new-version ' + version;
-      }
-      if (!version.startsWith('--')) version = '--' + version;
+if (modified.workspaces) {
+  modified.workspaces.packages = modified.workspaces.packages?.filter(
+    x => !x.startsWith('..') && !x.includes('/build'),
+  );
+}
+function restoreWorkspace() {
+  console.log('Restoring Package.json'); // eslint-disable-line no-console
+  writeFileSync(`${baseDir}/package.json`, pkgJson);
+}
 
-      console.log(['version', version]);
-      child = spawn(`yarn`, ['version', version], {
-        cwd: process.cwd(),
-        stdio: ['inherit', 'inherit', 'inherit', 'inherit'],
-      });
-    } else {
-      child = spawn(
-        `lerna`,
-        [
-          'version',
-          process.argv[2] ?? 'patch',
-          '--conventional-commits',
-          '--no-push',
-          '--exact',
-          '--force-publish',
-        ],
-        { cwd: process.cwd(), stdio: ['inherit', 'inherit', 'inherit', 'inherit'] },
-      );
+(async () => {
+  console.log('Removing external workspaces from package.json at: ', baseDir); // eslint-disable-line no-console
+
+  if (!modified.workspaces?.packages?.length) {
+    let versionArgs = [process.argv[2] ?? '--patch'];
+    if (versionArgs[0].match(/^\d/)) {
+      versionArgs.unshift('--new-version');
     }
-    await new Promise((resolve, reject) => {
-      child.once('close', resolve);
-      child.once('exit', resolve);
-      child.once('error', reject);
+    if (!versionArgs[0].startsWith('--')) versionArgs[0] = '--' + versionArgs[0];
+
+    spawnSync(`yarn`, ['version', ...versionArgs], {
+      cwd: process.cwd(),
+      stdio: ['inherit', 'inherit', 'inherit', 'inherit'],
     });
-  } finally {
-    console.log('Restoring Package.json'); // eslint-disable-line no-console
-    writeFileSync(`${baseDir}/package.json`, pkgJson);
+  } else {
+    writeFileSync(`${baseDir}/package.json`, JSON.stringify(modified, null, 2));
+    process.once('beforeExit', restoreWorkspace);
+    process.once('exit', restoreWorkspace);
+    process.once('SIGINT', restoreWorkspace);
+    process.once('SIGTERM', restoreWorkspace);
+    spawnSync(
+      `lerna`,
+      [
+        'version',
+        process.argv[2] ?? 'patch',
+        '--conventional-commits',
+        '--no-push',
+        '--exact',
+        '--force-publish',
+      ],
+      { cwd: process.cwd(), stdio: ['inherit', 'inherit', 'inherit', 'inherit'] },
+    );
+    restoreWorkspace();
   }
 })().catch(console.error);
